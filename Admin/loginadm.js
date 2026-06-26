1b043501d6dd13804e599a3383c95e76ce04de18
jonathan lopez <yona50029030@gmail.com>
2026-06-26 18:47:01 -0300

    refactor login page structure and enhance JavaScript functionality

--- Admin/loginadm.js
+++ Admin/loginadm.js
@@ -1,7 +1,5 @@
-  setTimeout(() => {
-
+setTimeout(() => {
   const loader = document.getElementById("loader");
-
   loader.style.opacity = "0";
 
   setTimeout(() => {
@@ -9,4 +7,16 @@
     document.getElementById("main").style.display = "flex";
   }, 1000);
 
-}, 3000); // 3 segundos
+}, 3000);
+
+const boton = document.getElementById("btnIngresar");
+
+boton.addEventListener("click", function() {
+    const usuario = document.getElementById("user").value;
+    const password = document.getElementById("password").value;
+    
+    console.log("Usuario:", usuario);
+    console.log("Password:", password);
+});
+
+if (usuario === "admin" && password === " ")
